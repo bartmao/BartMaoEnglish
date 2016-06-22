efc1b0f5c2aeeb645db75b640e8dc3deab61972f
bartmao <maochen.mars@qq.com>
2016-06-22 17:21:41 +0800

    video test .. succeed

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -1,8 +1,13 @@
 "use strict"
 
-var video = $('video')[0];
-video.playbackRate = 4;
-video.addEventListener('ended', function(e){
-    video.src = "http://localhost:3000/videos/Friends.S01E02.webm";
-    video.play();
-},false)
+var video1 = $('#video1')[0];
+var video2 = $('#video2')[0];
+video1.load();
+video2.load();
+video1.play();
+//video.playbackRate = 4;
+video1.addEventListener('ended', function (e) {
+    $('#video2').show();
+    var video2 = $('#video2')[0];
+    video2.play();
+}, false);
