b455ffebc0650706af0b009faea619e254a40051
bartmao <maochen.mars@qq.com>
2016-06-26 17:15:54 +0800

    revert bin folder

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', tests);
+app.use('/', videos);
 app.use('/audios', routes);
 app.use('/users', users);
 app.use('/test', tests);
